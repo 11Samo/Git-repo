caab86ba460387538e47365b2fa8c456cf9702ce
11Samo <samuel.kurnas@gmail.com>
2017-08-06 19:43:31 +0200

    Zmena

--- PokusnaApka3/PokusnaApka3/Program.cs
+++ PokusnaApka3/PokusnaApka3/Program.cs
@@ -11,7 +11,6 @@ namespace PokusnaApka3
         static void Main(string[] args)
         {
             Console.WriteLine("Pokusny vypis Master");
-            Console.WriteLine("Dalsi vypis z vedlajsej vetvy");
         }
     }
 }
